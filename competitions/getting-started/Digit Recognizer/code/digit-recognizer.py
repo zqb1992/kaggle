1f25aa6f30bc66676c83176d34232498c4b28160
zqb1992 <zqb920954494@163.com>
2018-10-26 17:49:42 +0800

    参数修改

--- competitions/getting-started/Digit Recognizer/code/digit-recognizer.py
+++ competitions/getting-started/Digit Recognizer/code/digit-recognizer.py
@@ -11,7 +11,12 @@
 
 import pandas as pd
 import numpy as np
+import csv
+from sklearn.decomposition import PCA
 
+COMPONENT_NUM = 40
+
+#加载数据
 def load_data():
     dataTrain = pd.read_csv(r"../data/train.csv")
     dataTest = pd.read_csv(r"../data/test.csv")
@@ -22,7 +27,50 @@ def load_data():
 
     return train_data,train_label,test_data
 
-
-
-
 load_data()
+
+#pca处理
+def DATA_PCA(train_data,test_data,component_num):
+    trainData = np.array(train_data)
+    testData = np.array(test_data)
+    pca = PCA(n_components=component_num,whiten=TRUE)
+    pca.fit(trainData)
+    #输出所选择维数所占的方差比
+    print(sum(pca.explained_variance_ratio_))
+    #转化训练集
+    train_data = pca.transform(trainData)
+    test_data = pca.transform((testDataata))
+    return train_data,test_data
+
+#模型训练
+def svm_model(train_data,train_label):
+    print('Train SVM...')
+    svc = SVC(C=4, kernel='rbf')
+    svc.fit(train_data, train_label)
+    return svc
+
+#保存数据
+def SaveResult(csvName,result):
+    with open(csvName, 'w') as writer:
+        writer.write('"ImageId","Label"\n')
+        count = 0
+        for res in result:
+            count += 1
+            writer.write(str(count) + ',"' + str(res) + '"\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- competitions/getting-started/Digit Recognizer/code/sklearn-pca-svm.py
+++ competitions/getting-started/Digit Recognizer/code/sklearn-pca-svm.py
@@ -6,10 +6,13 @@
 @file:sklearn-pca-svm.py
 @time:2018/10/25
 """
-
-import numpy
+import time
+import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
+
+startTime = time.time()
 
 COMPONENT_NUM = 35
 
@@ -27,19 +30,37 @@ print('Loaded ' + str(len(train_label)))
 
 #pca processing
 print('Reduction...')
-train_label = numpy.array(train_label)
-train_data = numpy.array(train_data)
+train_label = np.array(train_label)
+train_data = np.array(train_data)
+# pca = PCA()
 pca = PCA(n_components=COMPONENT_NUM, whiten=True)
 pca.fit(train_data)
+
+print(sum(pca.explained_variance_ratio_))
+
 train_data = pca.transform(train_data)
+
+#将降维后的训练集分为训练集和验证集两部分
+train_data,dev_data,train_label,dev_label = train_test_split(train_data,train_label,test_size=0.1,random_state=1)
+
+print(train_data.shape)
+print(dev_data.shape)
+
 
 #svm
 print('Train SVM...')
-svc = SVC()
+svc = SVC(C=5, kernel='rbf')
 svc.fit(train_data, train_label)
 
 
+#交叉验证
+pre_dev_label = svc.predict(dev_data)
+zeroLabel = pre_dev_label-dev_label
+rightCount = np.sum(zeroLabel == 0)
+print('the right rate is:',float(rightCount)/len(zeroLabel))
 
+endTime=time.time()
+print(u"耗时：%f s" %(endTime-startTime))
 
 print('Read testing data...')
 with open('../data/test.csv', 'r') as reader:
@@ -51,7 +72,7 @@ with open('../data/test.csv', 'r') as reader:
 print('Loaded ' + str(len(test_data)))
 
 print('Predicting...')
-test_data = numpy.array(test_data)
+test_data = np.array(test_data)
 test_data = pca.transform(test_data)
 predict = svc.predict(test_data)
 
@@ -63,3 +84,5 @@ with open('../out/digit_recognizer.csv', 'w') as writer:
         count += 1
         writer.write(str(count) + ',"' + str(p) + '"\n')
 
+endTime=time.time()
+print(u"耗时：%f s" %(endTime-startTime))
